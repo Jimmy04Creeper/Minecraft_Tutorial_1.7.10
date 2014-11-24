d7384d952ea6005dbed27f26465544dbf12bd7cf
jimmy <vickybrett@ymail.com>
2014-11-24 02:20:54 -1100

    Added base files to test github.

---
+++ src/jimmy/tutorial/MainMod.java
@@ -0,0 +1,13 @@
+package jimmy.tutorial;
+
+import jimmy.tutorial.util.Details;
+import cpw.mods.fml.common.Mod;
+
+/**
+ * Main Tutorial Mod File.
+ * @author Jimmy04Creeper
+ */
+@Mod(modid = Details.MODID, version = Details.VERSION)
+public class MainMod {
+
+}
---
+++ src/jimmy/tutorial/util/Details.java
@@ -0,0 +1,11 @@
+package jimmy.tutorial.util;
+
+/**
+ * Details.class holds basic stuff for the Mod like vesrion, name, title etc.
+ * @author Jimmy04Creeper
+ */
+public abstract class Details {
+    public static final String MODID = "DimensionTutorial";
+    public static final String VERSION = "1.0";
+    public static final String MCVERSION = "1.7.10";
+}
